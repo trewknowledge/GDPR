680d00ea18f85bd3189c1021f9b6824c2283c217
Fernando Claussen <fernandoclaussen@gmail.com>
2018-01-12 12:29:41 -0500

    Load the right template and fire an email. INCOMPLETE

--- includes/class-gdpr-notifications.php
+++ includes/class-gdpr-notifications.php
@@ -42,6 +42,15 @@ class GDPR_Notification {
 	protected $version;
 
 	/**
+	 * The template class instance.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $template    The template class instance.
+	 */
+	protected $template;
+
+	/**
 	 * Define the core functionality of the plugin.
 	 *
 	 * Set the plugin name and the plugin version that can be used throughout the plugin.
@@ -52,9 +61,57 @@ class GDPR_Notification {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+		$this->load_dependencies();
 
 	}
 
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - GDPR_Loader. Orchestrates the hooks of the plugin.
+	 * - GDPR_i18n. Defines internationalization functionality.
+	 * - GDPR_Admin. Defines all hooks for the admin area.
+	 * - GDPR_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-gdpr-templates.php';
+
+		$this->template = new GDPR_Templates();
+
+	}
+
+	public function notify( $user, $type = null ) {
+		if ( ! is_a( $user, 'WP_User' ) ) {
+			if ( ! is_int( $user ) ) {
+				return;
+			}
+			$user = get_user_by( 'ID', $user );
+		}
+
+		$content = $this->template::get_template_html( 'email/request-to-be-forgotten.php', array(
+			'user' => $user,
+		) );
+		error_log($content);
+
+		wp_mail( $user->user_email,
+			'Your preferences have been changed',
+			$content,
+			array('Content-Type: text/html; charset=UTF-8')
+		);
+	}
 
 
 }
