a47c42e0ec8639d83cbf1ddc99ab10521df8df2f
Fernando Claussen <fernandoclaussen@gmail.com>
2018-01-15 17:42:03 -0500

    Adding the right to access and right to be forgotten buttons

--- public/class-gdpr-public.php
+++ public/class-gdpr-public.php
@@ -51,7 +51,43 @@ class GDPR_Public {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+		$this->init();
 
+	}
+
+	private function init() {
+		add_shortcode( 'gdpr-forget-me', array($this, 'right_to_be_forgotten_button') );
+		add_shortcode( 'gdpr-right-to-access', array($this, 'right_to_access_button') );
+	}
+
+	public function right_to_be_forgotten_button() {
+		$user = get_current_user_id();
+		if ( ! $user ) {
+			return;
+		}
+
+		ob_start();
+		$classes = apply_filters( 'gdpr_button_class', 'gdpr-button' );
+		$text = apply_filters( 'gdpr_right_to_be_forgotten_text', __( 'Forget me', 'gdpr' ) );
+		?>
+			<button class="gdpr-right-to-be-forgotten <?php echo esc_attr( $classes ); ?>" data-nonce="<?php echo wp_create_nonce( 'request_to_be_forgotten' ) ?>"><?php echo esc_html( $text ); ?></button>
+		<?php
+		return ob_get_clean();
+	}
+
+	public function right_to_access_button() {
+		$user = get_current_user_id();
+		if ( ! $user ) {
+			return;
+		}
+
+		ob_start();
+		$classes = apply_filters( 'gdpr_button_class', 'gdpr-button' );
+		$text = apply_filters( 'gdpr_right_to_access_text', __( 'Download data', 'gdpr' ) );
+		?>
+			<button class="gdpr-right-to-access <?php echo esc_attr( $classes ); ?>" data-nonce="<?php echo wp_create_nonce( 'request_personal_data' ) ?>"><?php echo esc_html( $text ); ?></button>
+		<?php
+		return ob_get_clean();
 	}
 
 	/**
@@ -97,6 +133,12 @@ class GDPR_Public {
 		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/gdpr-public.js', array( 'jquery' ), $this->version, false );
+
+		wp_localize_script( $this->plugin_name, 'gdpr', array(
+			'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			'right_to_be_forgotten_confirmation_message' => esc_html__( 'Are you sure you want to remove all your personal information from our site?', 'gdpr' ),
+			'right_to_access_confirmation_message' => esc_html__( 'You are about to generate and download a file with all data we have about you. Are you sure you want to continue?', 'gdpr' ),
+		) );
 
 	}
 
--- public/js/gdpr-public.js
+++ public/js/gdpr-public.js
@@ -2,26 +2,54 @@
 	'use strict';
 
 	$(function(){
-		$('.gdpr-forget-me').click(function(e){
+
+		var uri = window.location.toString();
+		if (uri.indexOf("?") > 0) {
+				var clean_uri = uri.substring(0, uri.indexOf("?"));
+				window.history.replaceState({}, document.title, clean_uri);
+		}
+
+		$('.gdpr-right-to-be-forgotten').click(function(e){
 			e.preventDefault();
 
-			var confirmation = confirm( 'Are you sure you want to remove all your personal information from our site?' );
+			var confirmation = confirm( gdpr.right_to_be_forgotten_confirmation_message );
 			if ( ! confirmation ) {
 				return;
 			}
 
-			$(this).html('Loading');
-			console.log($(this).data('nonce'));
-
 			$.post(
-				GDPR.ajaxurl,
+				gdpr.ajaxurl,
 				{
-					action: 'request_to_be_forgotten',
+					action: 'process_right_to_be_forgotten',
 					nonce: $(this).data('nonce')
 				},
 				function(res) {
-					$(this).html('Done');
-					// location.reload();
+					console.log(res);
+				}
+			);
+		});
+
+		$('.gdpr-right-to-access').click(function(e){
+			e.preventDefault();
+
+			var confirmation = confirm( gdpr.right_to_access_confirmation_message );
+			if ( ! confirmation ) {
+				return;
+			}
+
+			$.post(
+				gdpr.ajaxurl,
+				{
+					action: 'process_right_to_access',
+					nonce: $(this).data('nonce')
+				},
+				function(res) {
+					// $('body').append('<a href="data:text/plain;charset=utf-8,' + encodeURIComponent(res.data) + '" download="data.xml">download</a>').click();
+					$('<a />', {
+						'href': 'data:text/plain;charset=utf-8,' + encodeURIComponent(res.data),
+						'download': 'data.xml',
+						'text': "click"
+					}).hide().appendTo("body")[0].click();
 				}
 			);
 		});
