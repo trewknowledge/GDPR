705ea42390d6d18689dde71256e74c75a84144f5
Fernando Claussen <fernandoclaussen@gmail.com>
2018-01-19 15:42:56 -0500

    Modal click events

--- public/js/gdpr-public.js
+++ public/js/gdpr-public.js
@@ -3,6 +3,50 @@
 
 	$(function(){
 
+		if ( $('.gdpr-consent-modal').length > 0 ) {
+			$('body').css('overflow', 'hidden');
+		}
+
+		$('.gdpr-disagree').click(function(e){
+			e.preventDefault();
+			const that = $(this);
+			$.post(
+				gdpr.ajaxurl,
+				{
+					action: 'disagree_with_terms',
+					nonce: $(this).data('nonce')
+				},
+				function(res) {
+					if ( res.success ) {
+						location.reload();
+					}
+				}
+			);
+		});
+
+		$('.gdpr-agree').click(function(e){
+			e.preventDefault();
+			const that = $(this);
+			$.post(
+				gdpr.ajaxurl,
+				{
+					action: 'agree_with_terms',
+					nonce: $(this).data('nonce'),
+					page: $(this).data('page')
+				},
+				function(res) {
+					if ( res.success ) {
+						that.closest('.gdpr-consent-modal').fadeOut(300, function(){
+							$(this).remove();
+							if ( $('.gdpr-consent-modal').length == 0 ) {
+								$('body').css('overflow', 'auto');
+							}
+						});
+					}
+				}
+			);
+		});
+
 		var uri = window.location.toString();
 		if (uri.indexOf("?") > 0) {
 				var clean_uri = uri.substring(0, uri.indexOf("?"));
@@ -22,9 +66,6 @@
 				{
 					action: 'process_right_to_be_forgotten',
 					nonce: $(this).data('nonce')
-				},
-				function(res) {
-					console.log(res);
 				}
 			);
 		});
