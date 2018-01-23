23b8bfb5384b4141608f45e3b1f379047ee241c5
Fernando Claussen <fernandoclaussen@gmail.com>
2018-01-23 14:32:01 -0500

    Updating the consents repeater to have a unique id

--- admin/partials/settings.php
+++ admin/partials/settings.php
@@ -16,25 +16,18 @@
 		return;
 	}
 
-	if ( isset( $_GET['settings-updated'] ) ) {
-		add_settings_error(
-			$this->plugin_name . '-notices',
-			esc_attr( 'settings-updated' ),
-			esc_html__( 'Settings Updated', 'gdpr' ),
-			'updated'
-		);
-	}
-
 	$pages = get_pages();
 	$options = get_option( 'gdpr_options', array() );
 ?>
 <div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	<?php if ( isset( $_GET['settings-updated'] ) ) : ?>
+		<div class="notice notice-success"><p><?php esc_html_e( 'Settings Updated', 'gdpr' ); ?></p></div>
+	<?php endif; ?>
 	<div id="poststuff">
 		<div id="post-body" class="metabox-holder columns-2">
 			<div id="post-body-content">
-				<?php	settings_errors( $this->plugin_name . '-notices' ); ?>
-				<form method="post">
+				<form method="post" action="<?php echo admin_url( 'admin.php?page=gdpr-settings&settings-updated=1' ) ?>">
 					<?php wp_nonce_field( 'gdpr_options_save' ); ?>
 					<div class="postbox">
 						<h2 class="hndle"><?php esc_html_e( 'General', 'gdpr' ); ?></h2>
@@ -78,38 +71,34 @@
 						</div>
 					</div>
 					<h3><?php esc_html_e( 'Consents', 'gdpr' ); ?></h3>
-					<p>You should have at the very least one consent filled for Terms of Service and one for Privacy Policy.</p>
-					<p>For every type of data you get from the user, you should have a box filled up.</p>
-					<p>This information shows up at the report the user can generate and download through the Right to Access component.</p>
-					<p><strong>Example:</strong></p>
-					<ul>
-						<li>You want to collect their emails for mailing purposes.</li>
-						<li>You implement a mailing solution like mailchimp.</li>
-						<li>You need to add another consent box below.</li>
-						<li>The title could be Email Collection</li>
-						<li>The description must be a detailed explanation of why you are getting that data, what do you want to do with it, how long will you keep it and if you are going to share that data with third parties.</li>
-					</ul>
+					<p><?php esc_html_e( 'For every type of data you get from the user, you should have a box filled up.', 'gdpr' ) ?></p>
+					<p><?php esc_html_e( 'The description must be a detailed explanation of why you are getting that data, what do you want to do with it, how long will you keep it and if you are going to share that data with third parties.', 'gdpr' ) ?></p>
 					<?php $consents = ( isset( $options['consents'] ) ) ? $options['consents'] : array(); ?>
 					<?php if ( ! empty( $consents ) ) : ?>
 						<?php $i = 0; ?>
-						<?php foreach ( $consents as $consent ) : ?>
+						<?php foreach ( $consents as $k => $v ) : ?>
 							<div class="postbox repeater">
 								<div class="inside">
 									<table class="form-table">
 										<tr>
 											<th><?php esc_html_e( 'Consent Title', 'gdpr' ) ?></th>
 											<td>
-												<input type="text" readonly class="regular-text" value="<?php echo esc_attr( $consent['title'] ) ?>" name="gdpr_options[consents][<?php echo $i; ?>][title]" id="">
+												<input type="text" data-id="title" class="regular-text" value="<?php echo esc_attr( $v['title'] ) ?>" name="gdpr_options[consents][<?php echo $i; ?>][title]" id="">
+											</td>
+										</tr>
+										<tr>
+											<th><?php esc_html_e( 'Unique ID', 'gdpr' ) ?></th>
+											<td>
+												<input type="text" data-id="id" readonly class="regular-text" value="<?php echo esc_attr( $k ) ?>" name="gdpr_options[consents][<?php echo $i; ?>][id]" id="">
 											</td>
 										</tr>
 										<tr>
 											<th><?php esc_html_e( 'Consent Description', 'gdpr' ) ?></th>
 											<td>
-												<textarea class="large-text" rows="4" name="gdpr_options[consents][<?php echo $i; ?>][description]"><?php echo esc_html( $consent['description'] ) ?></textarea>
+												<textarea class="large-text" rows="4" name="gdpr_options[consents][<?php echo $i; ?>][description]"><?php echo esc_html( $v['description'] ) ?></textarea>
 											</td>
 										</tr>
 									</table>
-									<a href="#" class="button delete-consent"><?php esc_html_e( 'Delete', 'gdpr' ); ?></a>
 								</div>
 							</div>
 						<?php $i++; ?>
