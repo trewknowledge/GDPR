e3f8013fd080dec9156a0e820004b4feed861468
Fernando Claussen <fernandoclaussen@gmail.com>
2018-03-06 14:54:50 -0500

    Adding a comment cookies section if comments are enabled

--- public/partials/cookie-preferences.php
+++ public/partials/cookie-preferences.php
@@ -26,13 +26,15 @@
 			</header>
 			<div class="content">
 				<ul class="tabs">
-					<li>
-						<button type="button" class="active" data-target="your-privacy"><?php esc_html_e( 'Your Privacy', 'gdpr' ); ?></button></li>
+					<li><button type="button" class="active" data-target="your-privacy"><?php esc_html_e( 'Your Privacy', 'gdpr' ); ?></button></li>
 					<?php
 						foreach ( $tabs as $key => $tab ) {
 							echo '<li><button type="button" data-target="' . esc_attr( $key ) . '" ' . '>' . esc_html( $tab['name'] ) . '</button></li>';
 						}
 					?>
+					<?php if ( 'open' === get_option( 'default_comment_status' ) ): ?>
+						<li><button type="button" data-target="comment-cookies"><?php esc_html_e( 'Comment Cookies', 'gdpr' ); ?></button></li>
+					<?php endif; ?>
 				</ul>
 				<div class="tab-content">
 					<div class="your-privacy active">
@@ -90,6 +92,37 @@
 							</div>
 						</div>
 					<?php endforeach; ?>
+					<?php if ( 'open' === get_option( 'default_comment_status' ) ): ?>
+						<div class="comment-cookies">
+							<header>
+								<h4><?php esc_html_e( 'Comment Cookies', 'gdpr' ); ?></h4>
+								<?php
+									$site_cookies = array();
+									$enabled = true;
+									$cookies_used = array( 'comment_author' );
+									foreach ( $cookies_used as $cookie ) {
+										$site_cookies[] = trim( $cookie );
+										if ( ! empty( $approved_cookies ) ) {
+											if ( ! in_array( trim( $cookie ), $approved_cookies ) ) {
+												$enabled = false;
+											}
+										}
+									}
+								?>
+								<label class="gdpr-switch">
+									<input type="checkbox" name="approved_cookies" value="<?php echo esc_attr( json_encode( $site_cookies ) ) ?>" <?php echo ( $enabled ? 'checked' : '' ); ?>>
+									<span class="gdpr-slider round"></span>
+								</label>
+							</header>
+							<div class="info">
+								<p><?php esc_html_e( 'Comment cookies are convenience cookies that temporarily store data so you don\'t have to type it on our comment form all the time.', 'gdpr' ); ?></p>
+								<strong><?php esc_html_e( 'Cookies used', 'gdpr' ); ?></strong>
+								<div class="cookies-used">
+									<span>comment_author_{$hash}, comment_author_email_{$hash}, comment_author_url_{$hash}</span>
+								</div>
+							</div>
+						</div>
+					<?php endif ?>
 				</div>
 			</div>
 			<footer>
