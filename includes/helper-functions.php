7071dfdcf62e06a55db926693f8b127564dcab4e
Fernando Claussen <fernandoclaussen@gmail.com>
2018-03-26 13:33:29 -0400

    Adding a helper function that checks if a cookie is allowed or not.

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -11,3 +11,16 @@ if ( ! function_exists( 'gdpr_request_form' ) ) {
 		GDPR_Requests_Public::request_form( $type );
 	}
 }
+
+if ( ! function_exists( 'is_allowed_cookie') ) {
+	function is_allowed_cookie( $cookie_name ) {
+		if ( isset( $_COOKIE['gdpr_approved_cookies'] ) ) {
+			$allowed_cookies = json_decode( wp_unslash( $_COOKIE['gdpr_approved_cookies'] ), true );
+			if ( in_array( $cookie_name, $allowed_cookies ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
