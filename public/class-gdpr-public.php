e929d57d66782e02bdb1ee4ef1f3d7ef6dce4012
Fernando Claussen <fernandoclaussen@gmail.com>
2018-03-29 15:33:44 -0400

    Change reconsent window to get the formatted content

--- public/class-gdpr-public.php
+++ public/class-gdpr-public.php
@@ -216,7 +216,9 @@ class GDPR_Public {
 				<div class="gdpr-consent-modal-content">
 					<h3><?php esc_html_e( 'Our Privacy Policy has been updated.', 'gdpr' ); ?></h3>
 					<h4><?php esc_html_e( 'To continue using the site you need to read the revised version and agree to the terms.', 'gdpr' ); ?></h4>
-					<textarea readonly><?php echo $page_obj->post_content; ?></textarea>
+					<div class="privacy-viewer">
+						<?php echo apply_filters( 'the_content', $page_obj->post_content ); ?>
+					</div>
 					<div class="gdpr-consent-buttons">
 						<a href="#" class="gdpr-agree" data-nonce="<?php echo wp_create_nonce( 'user_agree_with_terms' ) ?>"><?php esc_html_e( 'Agree', 'gdpr' ); ?></a>
 						<a href="#" class="gdpr-disagree" data-nonce="<?php echo wp_create_nonce( 'user_disagree_with_terms' ) ?>"><?php esc_html_e( 'Disagree', 'gdpr' ); ?></a>
