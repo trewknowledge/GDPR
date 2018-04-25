453d645673e75e60a78576c17d2ac9af4e47d1c7
mykola <ceo@mgm.cv.ua>
2018-04-25 20:59:42 +0300

    Fix admin email

--- templates/email/delete-resolved-notification.php
+++ templates/email/delete-resolved-notification.php
@@ -3,8 +3,8 @@
 echo sprintf(
 /* translators: 6-digit token for audit log */
     esc_html__(
-        'User account %s has been closed.
+        ' Some user account has been closed.
 
-Audit Token: %s', 'gdpr' ),
+', 'gdpr' ),
    $args['user'], $args['token']
 );
--- templates/email/export-data-resolved-notification.php
+++ templates/email/export-data-resolved-notification.php
@@ -3,7 +3,7 @@
 echo sprintf(
 /* translators: 6-digit token for audit log */
     esc_html__(
-        'User account  %s has exported their data using the Download My Data GDPR functionality.
+        'Some user account has exported their data using the Download My Data GDPR functionality.
 ', 'gdpr' ),
     $args['user']
 );
