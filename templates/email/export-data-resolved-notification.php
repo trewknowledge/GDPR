bd117fc014d19281fbc5e02a4ce1a621f5011098
Fernando Claussen <fernandoclaussen@gmail.com>
2018-04-27 16:37:45 -0400

    Removing unnecessary files

--- templates/email/export-data-resolved-notification.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo sprintf(
-/* translators: 6-digit token for audit log */
-    esc_html__(
-        'Some user account has exported their data using the Download My Data GDPR functionality.
-', 'gdpr' ),
-    $args['user']
-);
