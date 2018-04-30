8a0a0ab02737720bd17e77e5e5c451719f6e3047
Fernando Claussen <fernandoclaussen@gmail.com>
2018-04-30 11:18:37 -0400

    Leaving the work to the_content filter

--- public/partials/reconsent-modal.php
+++ public/partials/reconsent-modal.php
@@ -3,7 +3,7 @@
 		<h3><?php esc_html_e( 'Our Privacy Policy has been updated.', 'gdpr' ); ?></h3>
 		<h4><?php esc_html_e( 'To continue using the site you need to read the revised version and agree to the terms.', 'gdpr' ); ?></h4>
 		<div class="gdpr-privacy-viewer">
-			<?php echo wp_kses_post( apply_filters( 'the_content', $page_obj->post_content ) ); ?>
+			<?php echo apply_filters( 'the_content', $page_obj->post_content ); ?>
 		</div>
 		<div class="gdpr-consent-buttons">
 			<a href="#" class="gdpr-agree" data-nonce="<?php echo esc_attr( wp_create_nonce( 'user_agree_with_terms' ) ); ?>"><?php esc_html_e( 'Agree', 'gdpr' ); ?></a>
