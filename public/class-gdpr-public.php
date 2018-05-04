9472156de5df017fcf6aff0b5015098cdf8f5d8b
Fernando Claussen <fernandoclaussen@gmail.com>
2018-05-04 17:03:23 -0400

    Display the privacy preferences even when no cookies were registered.

--- public/class-gdpr-public.php
+++ public/class-gdpr-public.php
@@ -134,10 +134,8 @@ class GDPR_Public {
 	public function privacy_bar() {
 		$content             = get_option( 'gdpr_cookie_banner_content', '' );
 		$tabs                = get_option( 'gdpr_cookie_popup_content', array() );
-		$link_label          = get_option( 'gdpr_cookie_banner_privacy_policy_link_label', '' );
-		$privacy_policy_page = get_option( 'gdpr_privacy_policy_page', 0 );
 
-		if ( empty( $content ) || empty( $tabs ) ) {
+		if ( empty( $content ) ) {
 			return;
 		}
 
@@ -155,10 +153,6 @@ class GDPR_Public {
 		$privacy_policy_page = get_option( 'gdpr_privacy_policy_page', 0 );
 		$approved_cookies = isset( $_COOKIE['gdpr']['allowed_cookies'] ) ? json_decode( wp_unslash( $_COOKIE['gdpr']['allowed_cookies'] ) ) : array();
 		$user_consents = isset( $_COOKIE['gdpr']['consent_types'] ) ? json_decode( wp_unslash( $_COOKIE['gdpr']['consent_types'] ) ) : array();
-		$tabs = get_option( 'gdpr_cookie_popup_content', array() );
-		if ( empty( $tabs ) ) {
-			return;
-		}
 
 		include plugin_dir_path( __FILE__ ) . 'partials/privacy-preferences-modal.php';
 	}
