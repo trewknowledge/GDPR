4e8911f66d18dc42e167a0b405bd56f31a4eeed5
Matthew Farlymn <matthewfarlymn@gmail.com>
2018-05-22 17:22:26 -0400

    Privacy bar style changes

--- public/partials/privacy-bar.php
+++ public/partials/privacy-bar.php
@@ -18,6 +18,8 @@
 			<p>
 				<?php echo nl2br( wp_kses_post( $content ) ); ?>
 			</p>
+		</div>
+		<div class="gdpr-right">
 			<ul class="gdpr-cookie-categories">
 				<?php foreach ( $registered_cookies as $cookie_cat_id => $cookie_cat ): ?>
 					<?php
@@ -26,14 +28,12 @@
 							continue;
 						}
 					?>
-					<li>
+					<li class="gdpr-cookie-categories-item">
 						<input type="checkbox" id="gdpr-cookie-category-<?php echo esc_attr( $cookie_cat_id );	?>" class="gdpr-cookie-category" data-category="<?php echo esc_attr( $cookie_cat_id ); ?>" name="allowed_cookie_categories" value="<?php echo esc_attr( $cookie_cat['name'] ); ?>" <?php checked( $enabled, true ); ?> <?php echo ( 'required' === $cookie_cat['status'] ) ? 'disabled' : ''; ?>>
 						<label for="gdpr-cookie-category-<?php echo esc_attr( $cookie_cat_id ); ?>"><?php echo esc_html( $cookie_cat['name'] ); ?></label>
 					</li>
 				<?php endforeach ?>
 			</ul>
-		</div>
-		<div class="gdpr-right">
 			<button class="gdpr-preferences" type="button"><?php esc_html_e( 'Privacy Preferences', 'gdpr' ); ?></button>
 			<button class="gdpr-agreement" type="button"><?php echo esc_html( $button_text ); ?></button>
 		</div>
