ada46d81185fa0f1a88e3604b20cb2e2b79770fe
Matthew Farlymn <matthewfarlymn@gmail.com>
2018-05-23 16:01:03 -0400

    Redeveloped “policy” bard

--- public/partials/reconsent-bar.php
+++ public/partials/reconsent-bar.php
@@ -13,17 +13,26 @@
 ?>
 
 <div class="gdpr gdpr-reconsent-bar" style="/*display:none;*/">
-	<div class="gdpr-contained-wrapper">
-		<p class="h5"><?php esc_html_e( 'Some of our policies have been updated', 'gdpr' ); ?></p>
-		<?php foreach ( $updated_consents as $consent_id => $consent ): ?>
-			<div class="gdpr-policy-list">
-				<p><?php echo esc_html( $consent['name'] ); ?></p>
-				<div class="gdpr-buttons">
-					<a href="<?php echo esc_url( get_permalink( $consent['policy-page'] ) ); ?>" target="_blank"><?php esc_html_e( 'View', 'gdpr' ) ?></a>
-					<button class="gdpr-agree" type="button"><?php esc_html_e( 'Agree', 'gdpr' ); ?></button>
-				</div>
+	<div class="gdpr-wrapper">
+		<div class="gdpr-content">
+			<p><?php esc_html_e( 'Some of our policies have been updated', 'gdpr' ); ?></p>
+			<div class="gdpr-policy-pages">
+				<?php foreach ( $updated_consents as $consent_id => $consent ): ?>
+					<span class="gdpr-policy-pages-item"><a href="<?php echo esc_url( get_permalink( $consent['policy-page'] ) ); ?>" target="_blank"><?php esc_html_e( 'View', 'gdpr' ) ?></a> <?php echo esc_html( $consent['name'] ); ?> changes</span>
+				<?php endforeach ?>
 			</div>
-		<?php endforeach ?>
-
+		</div>
+		<div class="gdpr-right">
+			<ul class="gdpr-policy-list">
+				<?php foreach ( $updated_consents as $consent_id => $consent ) : ?>
+					<li class="gdpr-policy-list-item">
+						<input type="checkbox" id="gdpr-policy-item-<?php echo esc_attr( $consent_id ); ?>" class="gdpr-policy-item" data-category="<?php echo esc_attr( $consent_id ); ?>" name="allowed_cookie_categories" value="<?php echo esc_attr( $consent['name'] ); ?>" checked>
+						<label for="gdpr-policy-item-<?php echo esc_attr( $consent_id ); ?>"><?php echo esc_html( $consent['name'] ); ?></label>
+					</li>
+				<?php endforeach ?>
+			</ul>
+			<div class="gdpr-buttons">
+				<button class="gdpr-agreement" type="button"><?php esc_html_e( 'I Agree', 'gdpr' ); ?></button>
+			</div>
 	</div>
 </div>
