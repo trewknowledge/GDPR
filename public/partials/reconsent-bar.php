71c60aa134934aae11063ee2f04852cc64ef5258
Matthew Farlymn <matthewfarlymn@gmail.com>
2018-05-24 10:52:05 -0400

    Adjustments to privacy/policy bar layout.

--- public/partials/reconsent-bar.php
+++ public/partials/reconsent-bar.php
@@ -15,12 +15,7 @@
 <div class="gdpr gdpr-reconsent-bar" style="/*display:none;*/">
 	<div class="gdpr-wrapper">
 		<div class="gdpr-content">
-			<p><?php esc_html_e( 'Some of our policies have been updated', 'gdpr' ); ?></p>
-			<div class="gdpr-policy-pages">
-				<?php foreach ( $updated_consents as $consent_id => $consent ): ?>
-					<span class="gdpr-policy-pages-item"><a href="<?php echo esc_url( get_permalink( $consent['policy-page'] ) ); ?>" target="_blank"><?php esc_html_e( 'View', 'gdpr' ) ?></a> <?php echo esc_html( $consent['name'] ); ?> changes</span>
-				<?php endforeach ?>
-			</div>
+			<p><?php esc_html_e( 'Some of our policies have been updated. Please make sure to select the "View" link next each item in order to view changes before agreeing.', 'gdpr' ); ?></p>
 		</div>
 		<div class="gdpr-right">
 			<ul class="gdpr-policy-list">
@@ -28,6 +23,7 @@
 					<li class="gdpr-policy-list-item">
 						<input type="checkbox" id="gdpr-policy-item-<?php echo esc_attr( $consent_id ); ?>" class="gdpr-policy-item" data-category="<?php echo esc_attr( $consent_id ); ?>" name="allowed_cookie_categories" value="<?php echo esc_attr( $consent['name'] ); ?>" checked>
 						<label for="gdpr-policy-item-<?php echo esc_attr( $consent_id ); ?>"><?php echo esc_html( $consent['name'] ); ?></label>
+						<a class="gdpr-policy-link" href="<?php echo esc_url( get_permalink( $consent['policy-page'] ) ); ?>" target="_blank">[<?php esc_html_e( 'View', 'gdpr' ) ?>]</a>
 					</li>
 				<?php endforeach ?>
 			</ul>
