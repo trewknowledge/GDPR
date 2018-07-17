d0f204cbd73470776fed9a5e91b1570d085ea601
Fernando Claussen <fernandoclaussen@gmail.com>
2018-07-17 15:30:55 -0400

    Adding more accents to the replacement list. Fix #199 (#219)

--- src/js/admin/gdpr-admin.js
+++ src/js/admin/gdpr-admin.js
@@ -11,8 +11,8 @@
 	    str = str.toLowerCase();
 
 	    // remove accents, swap ñ for n, etc
-	    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-	    var to   = "aaaaeeeeiiiioooouuuunc------";
+	    var from = "àáäâèéëêìíïîòóöôùúüûñňçčľĺšťžýď·/_,:;";
+	    var to   = "aaaaeeeeiiiioooouuuunnccllstzyd------";
 	    for (var i=0, l=from.length ; i<l ; i++) {
         str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
 	    }
