2601364174f653f0eb4a67ec6cea2781205d2aab
Yaniv Harush <harushy@gmail.com>
2018-08-12 13:10:33 +0300

    added the visitor's ip to the gdpr_is_eu_visitor

--- public/class-gdpr-public.php
+++ public/class-gdpr-public.php
@@ -222,7 +222,7 @@
 			if ( function_exists( 'geoip_continent_code_by_name' ) ) {
 				return ( geoip_continent_code_by_name( $this->get_visitor_ip() ) === 'EU' );
 			} else {
-				return apply_filters( 'geoip_is_eu_visitor', false );
+				return apply_filters( 'geoip_is_eu_visitor', false, $this->get_visitor_ip() );
 			}
 		}
 
