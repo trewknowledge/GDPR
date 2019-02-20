ad4227a1e8000521fa07bb16039a98980b2d3024
a6_chris <chris@a6software.co.uk>
2019-02-20 10:00:14 +0000

    PHPCS - rename sniff to VIP variant in class-gdpr-requests.php

--- includes/class-gdpr-requests.php
+++ includes/class-gdpr-requests.php
@@ -164,7 +164,7 @@ class GDPR_Requests {
 
 		$user = get_user_by( 'email', $email );
 
-		// phpcs:disable WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		// phpcs:disable WordPress.VIP.SlowDBQuery.slow_db_query_meta_key
 		if ( $user instanceof WP_User ) {
 			$meta_key = self::$plugin_name . "_{$type}_key";
 			update_option( 'gdpr_requests', $requests );
@@ -187,7 +187,7 @@ class GDPR_Requests {
 				);
 			}
 		}
-		// phpcs:enable WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		// phpcs:enable WordPress.VIP.SlowDBQuery.slow_db_query_meta_key
 
 		return true;
 	}
@@ -265,7 +265,7 @@ class GDPR_Requests {
 		/**
 		 * Remove user from the requests if it did not confirm in 2 days.
 		 */
-		// phpcs:disable WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		// phpcs:disable WordPress.VIP.SlowDBQuery.slow_db_query_meta_key
 		$user = get_user_by( 'email', $email );
 		if ( $user instanceof WP_User ) {
 			$meta_key = self::$plugin_name . '_' . $type . '_key';
@@ -296,7 +296,7 @@ class GDPR_Requests {
 				)
 			);
 		}
-		// phpcs:enable WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		// phpcs:enable WordPress.VIP.SlowDBQuery.slow_db_query_meta_key
 
 		update_option( 'gdpr_requests', $requests );
 
