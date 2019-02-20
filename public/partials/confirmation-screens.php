ac917bf659f9b69423e6a51478cb3dddf7d0d8a9
a6_chris <chris@a6software.co.uk>
2019-02-20 14:53:04 +0000

    Replace confirmation-screens with patched version from client-mu-plugins/gdpr

--- public/partials/confirmation-screens.php
+++ public/partials/confirmation-screens.php
@@ -1,4 +1,7 @@
 <?php
+
+declare( strict_types=1 );
+
 /**
  * The confirmation dialogs.
  *
@@ -9,53 +12,58 @@
  * @subpackage public
  * @author     Fernando Claussen <fernandoclaussen@gmail.com>
  */
-?>
 
-<?php
-$confirmation_title = '';
-$confirmation_text  = '';
-if ( isset( $_GET['notify'] ) && absint( $_GET['notify'] ) ) : // WPCS: Input var ok, CSRF ok.
+$notify = filter_input( INPUT_GET, 'notify', FILTER_SANITIZE_NUMBER_INT );
+$user_deleted = filter_input( INPUT_GET, 'user-deleted', FILTER_SANITIZE_NUMBER_INT );
+$request_confirmed = filter_input( INPUT_GET, 'request-confirmed', FILTER_SANITIZE_NUMBER_INT );
+$user_not_found = filter_input( INPUT_GET, 'user-not-found', FILTER_SANITIZE_NUMBER_INT );
+$request_key_not_found = filter_input( INPUT_GET, 'request-key-not-found', FILTER_SANITIZE_NUMBER_INT );
+$request_key_not_match = filter_input( INPUT_GET, 'request-key-not-match', FILTER_SANITIZE_NUMBER_INT );
+
+$modal_title = '';
+$modal_text = '';
+if ( isset( $notify ) && absint( $notify ) ) :
 	?>
 	<?php
-	if ( isset( $_GET['user-deleted'] ) ) { // WPCS: Input var ok, CSRF ok.
-		$confirmation_title = __( 'Your account', 'gdpr' );
-		if ( absint( $_GET['user-deleted'] ) ) { // WPCS: Input var ok, CSRF ok.
-			$confirmation_text = __( 'Your account has been closed. We are sorry to see you go.', 'gdpr' );
+	if ( isset( $user_deleted ) ) {
+		$modal_title = __( 'Your account', 'gdpr' );
+		if ( absint( $user_deleted ) ) {
+			$modal_text = __( 'Your account has been closed. We are sorry to see you go.', 'gdpr' );
 		} else {
-			$confirmation_text = __( 'Your request has been received and is being reviewed. You will receive an email when we are done.', 'gdpr' );
+			$modal_text = __( 'Your request has been received and is being reviewed. You will receive an email when we are done.', 'gdpr' );
 		}
 	}
-	if ( isset( $_GET['request-confirmed'] ) && absint( $_GET['request-confirmed'] ) ) { // WPCS: Input var ok, CSRF ok.
-		$confirmation_title = __( 'Request Received', 'gdpr' );
-		$confirmation_text  = __( 'Your request has been received. We will be in touch soon.', 'gdpr' );
+	if ( isset( $request_confirmed ) && absint( $request_confirmed ) ) {
+		$modal_title = __( 'Request Received', 'gdpr' );
+		$modal_text = __( 'Your request has been received. We will be in touch soon.', 'gdpr' );
 	}
-	if ( isset( $_GET['user-not-found'] ) && absint( $_GET['user-not-found'] ) ) { // WPCS: Input var ok CSRF ok.
-		$confirmation_title = __( 'Error!', 'gdpr' );
-		$confirmation_text  = __( 'User not found.', 'gdpr' );
+	if ( isset( $user_not_found ) && absint( $user_not_found ) ) {
+		$modal_title = __( 'Error!', 'gdpr' );
+		$modal_text = __( 'User not found.', 'gdpr' );
 	}
-	if ( isset( $_GET['request-key-not-found'] ) && absint( $_GET['request-key-not-found'] ) ) { // WPCS: Input var ok, CSRF ok.
-		$confirmation_title = __( 'Error!', 'gdpr' );
-		$confirmation_text  = __( 'We could not confirm the request key. It may be expired.', 'gdpr' );
+	if ( isset( $request_key_not_found ) && absint( $request_key_not_found ) ) {
+		$modal_title = __( 'Error!', 'gdpr' );
+		$modal_text = __( 'We could not confirm the request key. It may be expired.', 'gdpr' );
 	}
-	if ( isset( $_GET['request-key-not-match'] ) && absint( $_GET['request-key-not-match'] ) ) { // WPCS: Input var ok, CSRF ok.
-		$confirmation_title = __( 'Error!', 'gdpr' );
-		$confirmation_text  = __( 'The key used does not match the request key we have stored.', 'gdpr' );
+	if ( isset( $request_key_not_match ) && absint( $request_key_not_match ) ) {
+		$modal_title = __( 'Error!', 'gdpr' );
+		$modal_text = __( 'The key used does not match the request key we have stored.', 'gdpr' );
 	}
 	?>
 <?php endif ?>
 <div class="gdpr gdpr-general-confirmation">
-	<div class="gdpr-wrapper">
-		<header>
-			<div class="gdpr-box-title">
-				<h3><?php echo esc_attr( $confirmation_title ); ?></h3>
-				<span class="gdpr-close"></span>
-			</div>
-		</header>
-		<div class="gdpr-content">
-			<p><?php echo esc_html( $confirmation_text ); ?></p>
-		</div>
-		<footer>
-			<button class="gdpr-ok" data-callback="closeNotification"><?php esc_html_e( 'OK', 'gdpr' ); ?></button>
-		</footer>
-	</div>
+    <div class="gdpr-wrapper">
+        <header>
+            <div class="gdpr-box-title">
+                <h3><?php echo esc_attr( $modal_title ); ?></h3>
+                <span class="gdpr-close"></span>
+            </div>
+        </header>
+        <div class="gdpr-content">
+            <p><?php echo esc_html( $modal_text ); ?></p>
+        </div>
+        <footer>
+            <button class="gdpr-ok" data-callback="closeNotification"><?php esc_html_e( 'OK', 'gdpr' ); ?></button>
+        </footer>
+    </div>
 </div>
